37b7ad1699b40ea97effa17dd5affb9b5810aab0
Durvesh Bhalekar <durvesh.5.db@gmail.com>
2020-06-14 18:09:24 +0530

    Add Hamburger

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.getElementById("ham").addEventListener("click",loadham);
 
-function loadham(e){
+function loadham(){
+
+  document.getElementById("ham").classList.toggle("is-active");
+
   var olddis = document.getElementById("ham-content").style.display;
-
   var newdis = (olddis == "flex")?"none":"flex";
-
-
   document.getElementById("ham-content").style.display = newdis;
   console.log(olddis);
   console.log(newdis);
